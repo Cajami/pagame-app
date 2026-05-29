1b0043b6fad1b90ff386197fc638e90567356fb9
Javier Huiñocana Inoñan <javier2315@gmail.com>
2026-05-29 16:05:33 -0500

    refactor: rename package to com.cajami.pagame across all platforms

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.pagame.app.pagame"
+    namespace = "com.cajami.pagame"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -26,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.pagame.app.pagame"
+        applicationId = "com.cajami.pagame"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/cajami/pagame/MainActivity.kt
+++ android/app/src/main/kotlin/com/cajami/pagame/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.pagame.app.pagame
+package com.cajami.pagame
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
